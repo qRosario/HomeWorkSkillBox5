e0714f1621f52b7814548b0cb1d2c171b23ab07e
Daniil <danyastroev@mail.ru>
2022-07-06 21:41:32 +0300

    Добавьте файлы проекта.

---
+++ Dz2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Dz2
+{
+    internal class Program
+    {
+
+        static string[] WordSeparator(string text)
+        {
+            string[] separatedWords = text.Split(' ');
+
+            return separatedWords;
+        }
+
+        static string ReversWord(string[] text)
+        {
+            Array.Reverse(text);
+            string reverseWord = string.Join(" ", text);
+            return reverseWord;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string result = Console.ReadLine();
+            result = ReversWord(WordSeparator(result));
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ HomeWorkSkillBox5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HomeWorkSkillBox5
+{
+    internal class Program
+    {
+        static string[] WordSeparator(string text)
+        {
+            string[] separatedWords = text.Split(' ');
+
+            return separatedWords;
+        }
+
+        static void DataOutput(string[] text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.Write(text[i] + "\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string inputText = Console.ReadLine();
+            string[] result = WordSeparator(inputText);
+            DataOutput(result);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
